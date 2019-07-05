a71c803fd542c7b3e936f45691770327b13be9e3
Joshua Weinstein <jweinstein@splunk.com>
2019-07-04 18:55:24 -0700

    string work

---
+++ core/tis620str.h
@@ -0,0 +1,9 @@
+#ifndef THAISTR_CORE_TIS620STR_H
+#define THAISTR_CORE_TIS620STR_H
+
+#include <stdlib.h>
+#include "character-types.h"
+
+size_t thaistr_tis620_len(const tis620_t* string);
+
+#endif // THAISTR_CORE_TIS620STR_H
